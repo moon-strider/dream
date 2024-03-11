91436e160e6d332f1518e0ecb641deb97cd9a26f
Ilia Dikanskiy <92857699+moon-strider@users.noreply.github.com>
2024-03-11 16:23:41 +0300

    Initialize user_km_test

---
+++ common/art.py
@@ -0,0 +1,22 @@
+import re
+from common.combined_classes import TOPIC_GROUPS
+from common import utils
+
+
+SUPER_CONFIDENCE = 1.0
+HIGH_CONFIDENCE = 0.98
+DEFAULT_CONFIDENCE = 0.95
+BIT_LOWER_CONFIDENCE = 0.90
+ZERO_CONFIDENCE = 0.0
+
+ART_PATTERN = re.compile(r"\b(art|artist|drawing|painting|painter|gallery)(\.|\?|\b)", re.IGNORECASE)
+
+
+def check_about_art(user_uttr):
+    found_topics = utils.get_topics(user_uttr, probs=False, which="all")
+    if any([art_topic in found_topics for art_topic in TOPIC_GROUPS["art"]]):
+        return True
+    elif re.findall(ART_PATTERN, user_uttr["text"]):
+        return True
+    else:
+        return False
